ca8cd6eb78c79605ffa274dcdaf87b74f845977a
wox0 <wox0@protonmail.com>
2017-12-01 14:51:17 +0100

    new things

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        Debug.Log("loaded");
+
+        Application.LoadLevel(1);
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void Credits()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
