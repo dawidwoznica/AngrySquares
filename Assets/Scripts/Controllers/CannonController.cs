e3ab6f51b45b95f167a4f84528910640e9da390a
wox0 <wox0@protonmail.com>
2018-01-03 01:11:30 +0100

    PauseMenu added. Arrow buttons rework + pause button added.

--- Assets/Scripts/Controllers/CannonController.cs
+++ Assets/Scripts/Controllers/CannonController.cs
@@ -3,74 +3,73 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Windows.Speech;
 
 public class CannonController : MonoBehaviour {
 
 	Rigidbody2D _cannonBallRB;
-    public CannonMovementController CannonMovementController;
+	public CannonMovementController CannonMovementController;
 
 	private AudioSource _cannonShoot;
 
 /*    private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
-    private KeywordRecognizer keywordRecognizer;*/
+	private KeywordRecognizer keywordRecognizer;*/
 
 	// Use this for initialization
 	void Start ()
 	{
 		_cannonShoot = GetComponent<AudioSource>();
 
-     /*   keywordActions.Add("fire", Shoot);
-	    keywordActions.Add("shoot", Shoot);
-        keywordActions.Add("shot", Shoot);
-        keywordActions.Add("up", Up);
-	    keywordActions.Add("down", Down);
-        keywordActions.Add("upper", Up);
-        keywordActions.Add("lower", Down);
-	    keywordActions.Add("left", CannonMovementController.Left);
-	    keywordActions.Add("right", CannonMovementController.Right);
-	    keywordActions.Add("go left", CannonMovementController.Left);
-	    keywordActions.Add("go right", CannonMovementController.Right);
-	    keywordActions.Add("move left", CannonMovementController.Left);
-	    keywordActions.Add("move right", CannonMovementController.Right);
+	 /*   keywordActions.Add("fire", Shoot);
+		keywordActions.Add("shoot", Shoot);
+		keywordActions.Add("shot", Shoot);
+		keywordActions.Add("up", Up);
+		keywordActions.Add("down", Down);
+		keywordActions.Add("upper", Up);
+		keywordActions.Add("lower", Down);
+		keywordActions.Add("left", CannonMovementController.Left);
+		keywordActions.Add("right", CannonMovementController.Right);
+		keywordActions.Add("go left", CannonMovementController.Left);
+		keywordActions.Add("go right", CannonMovementController.Right);
+		keywordActions.Add("move left", CannonMovementController.Left);
+		keywordActions.Add("move right", CannonMovementController.Right);
 
-        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
-	    keywordRecognizer.OnPhraseRecognized += OnKeyRecognized;
-        keywordRecognizer.Start();*/
+		keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += OnKeyRecognized;
+		keywordRecognizer.Start();*/
 	}
-    
+	
    /* private void OnKeyRecognized(PhraseRecognizedEventArgs args)
-    {
-        Debug.Log("Keyword: "+ args.text);
-        keywordActions[args.text].Invoke();
-    }*/
+	{
+		Debug.Log("Keyword: "+ args.text);
+		keywordActions[args.text].Invoke();
+	}*/
 
 
-    // Update is called once per frame
-    void Update () {
+	// Update is called once per frame
+	void Update () {
 
 	}
 
 	public void Shoot()
 	{
-	    if (!GameManager.OptionsManager.IsSoundMuted)
-	    {
-	        _cannonShoot.Play();
-        }
-        GameManager.CannonManager.CannonShootEffect.Play();
+		if (!GameManager.OptionsManager.IsSoundMuted)
+		{
+			_cannonShoot.Play();
+		}
+		GameManager.CannonManager.CannonShootEffect.Play();
 		GameObject _cannonBallCopy = Instantiate(GameManager.CannonManager.CannonBall, GameManager.CannonManager.ShotPosition.position, transform.rotation);
 		_cannonBallRB = _cannonBallCopy.GetComponent<Rigidbody2D>();
 		_cannonBallRB.AddForce(transform.right * GameManager.CannonManager.FirePower);
 	}
 
 
-    public void Up()
-    {
-        transform.Rotate(0, 0, Time.deltaTime * GameManager.CannonManager.BarrelRotationSpeed);
-    }
+	public void Up()
+	{
+		transform.Rotate(0, 0, Time.deltaTime * GameManager.CannonManager.BarrelRotationSpeed);
+	}
 
-    public void Down()
-    {
-        transform.Rotate(0, 0, -Time.deltaTime * GameManager.CannonManager.BarrelRotationSpeed);
-    }
+	public void Down()
+	{
+		transform.Rotate(0, 0, -Time.deltaTime * GameManager.CannonManager.BarrelRotationSpeed);
+	}
 }
---
+++ Assets/Scripts/Controllers/PauseMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    public Canvas PauseMenuCanvas;
+
+    public void ShowPauseMenu()
+    {
+        PauseMenuCanvas.enabled = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuCanvas.enabled = false;
+    }
+
+    public void MainMenu()
+    {
+        GameObject.Find("BackgroundMusic").GetComponent<AudioSource>().Play();
+        GameObject.Find("BackgroundWind").GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene("scn_MainMenu", LoadSceneMode.Single);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("scn_Options", LoadSceneMode.Additive);
+    }
+}
