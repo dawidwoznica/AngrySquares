74bb5382c667d4dd5d7bb0f3a0b4f270fd2a83bc
wox0 <wox0@protonmail.com>
2018-01-25 10:30:46 +0100

    Levels set up

--- Assets/Scripts/Controllers/CannonBallController.cs
+++ Assets/Scripts/Controllers/CannonBallController.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class CannonBallController : MonoBehaviour {
 
-    public GameObject Explosion;
-    private AudioSource _boomSound;
-    private bool _afterCollision;
+	public GameObject Explosion;
+	private AudioSource _boomSound;
+	private bool _afterCollision;
 
-    void Awake()
-    {
-        _boomSound = GetComponent<AudioSource>();
-    }
+	void Awake()
+	{
+		_boomSound = GetComponent<AudioSource>();
+	}
 
 	void Start ()
 	{
-	    _afterCollision = false;
+		_afterCollision = false;
 	}
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (!_afterCollision)
-        {
-            Instantiate(Explosion, transform.position, transform.rotation);
-            //Destroy(other.gameObject, 3);
-            if (other.gameObject.layer == 8)
-            {
-                Destroy(other.gameObject);
-            }
-            if (!GameManager.OptionsManager.IsSoundMuted)
-            {
-                _boomSound.Play();
-            }
-            _afterCollision = true;
-            //gameObject.SetActive(false);
-            //Destroy(gameObject, 2);
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (!_afterCollision)
+		{
+			Instantiate(Explosion, transform.position, transform.rotation);
+			//Destroy(other.gameObject, 3);
+			if (other.gameObject.layer == 8)
+			{
+				Destroy(other.gameObject);
+			}
+			if (!GameManager.OptionsManager.IsSoundMuted)
+			{
+				_boomSound.Play();
+			}
+			if (!other.gameObject.tag.Equals("Bar"))
+			{
+				_afterCollision = true;
 
-        }
-    }
+			}
+			//gameObject.SetActive(false);
+			//Destroy(gameObject, 2);
+
+		}
+	}
 }
---
+++ Assets/Scripts/Controllers/OkButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OkButtonController : MonoBehaviour
+{
+
+    public Canvas HintUI;
+
+    public void HideUI()
+    {
+        HintUI.gameObject.SetActive(false);
+    }
+}
